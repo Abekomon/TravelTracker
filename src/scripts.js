616a573f93c31bb5f75bb5e2753a775308852292
Max Lange <max@langes.us>
2023-01-12 19:40:39 -0500

    Add full post cababilities for request form, as well as update the dashboard to have pending request, also adjusts layout to be tab-based

--- src/scripts.js
+++ src/scripts.js
@@ -13,7 +13,15 @@ const upcomingTripBox = document.getElementById('upcomingTrips');
 const pendingTripBox = document.getElementById('pendingTrips');
 const welcomeText = document.getElementById('welcome');
 const costText = document.getElementById('yearCost');
+const dashboard = document.getElementById('dashboard');
+const form = document.getElementById('form')
+const reqForm = document.getElementById('requestForm');
 const formDest = document.getElementById('requestDestination');
+const formDura = document.getElementById('requestDuration');
+const formDate = document.getElementById('requestDate');
+const formTrav = document.getElementById('requestTravelers');
+const dashNavTab = document.getElementById('dashNav');
+const reqNavTab = document.getElementById('reqNav');
 
 
 //Global Variables
@@ -37,17 +45,18 @@ function fetchStart() {
 function loadHandler() {
   displayWelcomeMessage();
   displayAllTrips();
-  generateFormOptions();
+  generateForm();
 }
 
 function generateRandomIndex() {
   return Math.floor(Math.random() * allTravelers.length);
 }
 
-function generateFormOptions() {
+function generateForm() {
   allDestinations.forEach(dest => {
     formDest.innerHTML += `<option value="${dest.id}">${dest.destination}</option>`
   })
+  formDate.setAttribute("min", currentDate.toISOString().split('T')[0])
 }
 
 function displayWelcomeMessage() {
@@ -55,7 +64,7 @@ function displayWelcomeMessage() {
 }
 
 function displayAllTrips() {
-  const userTrips = new Trip(currentUser, allTrips);
+  let userTrips = new Trip(currentUser, allTrips);
   displayTripType(userTrips, "past", pastTripBox);
   displayTripType(userTrips, "upcoming", upcomingTripBox);
   displayTripType(userTrips, "pending", pendingTripBox);
@@ -63,6 +72,7 @@ function displayAllTrips() {
 }
 
 function displayTripType(data, type, location) {
+  location.innerHTML = '';
   data[`${type}Trips`].forEach(trip => {
     const dest = new Destination(trip, allDestinations);
     location.innerHTML += 
@@ -84,6 +94,70 @@ function displayYTDCost(data) {
   costText.innerText = `Your total cost this year is: $${(Number(cost).toLocaleString())}`
 }
 
+function getFormData() {
+  return {
+    id: allTrips.length + 1,
+    userID: currentUser.id,
+    destinationID: Number(formDest.value),
+    travelers: Number(formTrav.value),
+    date: formDate.value.replaceAll("-", "/"),
+    duration: Number(formDura.value),
+    status: "pending",
+    suggestedActivities: [ ]
+  }
+}
+
+function postFormData() {
+  const postData = getFormData();
+  fetch("http://localhost:3001/api/v1/trips", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(response => {
+    if(response.ok){
+      return response.json;
+    } else {
+      throw new Error('Something went wrong!')
+    }
+  }).then(updateValues())
+    .catch(error => console.log(error))
+}
+
+function updateValues(){
+  fetch(`http://localhost:3001/api/v1/trips`)
+  .then(response => response.json())
+  .then(data => {
+    allTrips = data.trips;
+    displayAllTrips();
+  })
+}
+
+function changeView(){
+  dashboard.classList.toggle("hidden");
+  dashNavTab.classList.toggle("focused")
+  form.classList.toggle("hidden");
+  reqNavTab.classList.toggle("focused");
+}
+
 //Event Listeners
 
-window.addEventListener('load', fetchStart)
+window.addEventListener('load', fetchStart);
+
+dashNavTab.addEventListener('click', () => {
+  if(dashboard.classList.contains('hidden')){
+    changeView();
+  }
+})
+
+reqNavTab.addEventListener('click', () => {
+  if(form.classList.contains('hidden')) {
+    changeView();
+  }
+})
+
+reqForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  postFormData();
+});
